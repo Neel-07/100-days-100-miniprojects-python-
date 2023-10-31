726c15576540b85a69ed12f0e2ff458c968d61e3
Neel Mishra <neelmishra9125@gmail.com>
2023-10-31 07:51:24 +0530

    added table.py
    Signed-off-by: Neel Mishra <neelmishra9125@gmail.com>

---
+++ Programs/table.py
@@ -0,0 +1,35 @@
+# Python program to create a table
+
+from tkinter import *
+
+
+class Table:
+	
+	def __init__(self,root):
+		
+		# code for creating table
+		for i in range(total_rows):
+			for j in range(total_columns):
+				
+				self.e = Entry(root, width=20, fg='blue',
+							font=('Arial',16,'bold'))
+				
+				self.e.grid(row=i, column=j)
+				self.e.insert(END, lst[i][j])
+
+# take the data
+lst = [(1,'Vishal','Mumbai',19),
+	(2,'Ahana','Pune',18),
+	(3,'Ravi','Mumbai',20),
+	(4,'Devesh','Mumbai',21),
+	(5,'Shruti','Delhi',21)]
+
+# find total number of rows and
+# columns in list
+total_rows = len(lst)
+total_columns = len(lst[0])
+
+# create root window
+root = Tk()
+t = Table(root)
+root.mainloop()
